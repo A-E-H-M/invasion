be569ecb369bb041b57aac2aeb67ee43dfffa1f8
alexandria <alexandria.hmann@gmail.com>
2024-07-22 18:57:32 -0500

    Restructuring code

---
+++ example/invasion_example.cpp
@@ -0,0 +1,17 @@
+// Testing ground for things
+#include <string>
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+#include "game.h"
+#include "window.h"
+
+int main (){
+Game test;
+
+test.run();
+
+return 0;
+};
--- src/entities.cpp
+++ src/entities.cpp
@@ -6,29 +6,25 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 
-int main(){
-	
-// Window settings
-	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();				// Get the current desktop video mode
-	// Render window
-	int wWidth {1280};
-	int wHeight {920};
-	sf::RenderWindow window(sf::VideoMode(wWidth, wHeight, desktop.bitsPerPixel), "Alien Invasion");
-	window.setFramerateLimit(60);
+class ETexture {
+	public:
+		sf::Texture eTexture;
 
-	// Cat
-	sf::Texture texture;
-	texture.loadFromFile("cat.png");					
-	sf::Vector2 textureSize = texture.getSize();								// Get the size of the texture in pixels
-	
-	sf::Sprite sprite;
-	sprite.setTexture(texture);
-	float spriteScale {.25};
-	
-	sprite.scale(spriteScale, spriteScale);										// Rescale the size to be less than the 
-																				// original texture
+		//ETexture(const std::string& filename) :  { [] (eTexture.loadFromFile(filename)) };
+};
+
+class ESprite {
+	public:
+		sf::Sprite eSprite;
+		int eSpriteScale {0};
+
+		ESprite(const ETexture& eSpriteTexture, float scale) : eSpriteScale(scale) {
+			eSprite.setTexture(eSpriteTexture.eTexture);
+			eSprite.scale(eSpriteScale, eSpriteScale);
+		};
+};
+
 	sprite.setOrigin((textureSize.x/2)*spriteScale, (textureSize.y/2)*spriteScale); // Like setting the centroid for the sprite
 	sprite.setPosition((wWidth/2), (wHeight-textureSize.y*spriteScale));		// Setting the position based on screen
 
-return 0;
 }
